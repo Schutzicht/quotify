ca92a89f41f4279df3126b941b68f2cb10481261
Jorik Schut <jorikschut@MacBook-Air-van-Jorik.local>
2026-01-29 14:35:00 +0100

    Quotify Fix: Force fresh install (rm package-lock) & use stable Stripe API 2023-10-16

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2024-12-18.acacia',
+    apiVersion: '2023-10-16',
 });
 
 // Middleware
